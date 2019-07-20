254590833a8c9684d7e77da3edf8cf3f8d0c60ca
amfde <amfservicos@hotmail.com>
2019-07-19 21:34:22 -0300

    update

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,15 @@
 const itens = document.querySelectorAll('.collection-item')
 const quantidade = document.querySelectorAll('.badge')
 
-let lista_add = []
-
+//let lista_add = []
 
 itens.forEach((item)=>{
     item.addEventListener('click', (event)=>{              
         event.preventDefault()
-        lista_add.push(item.textContent)
-        lista = [new Set(lista_add)]
-        console.log(lista)
+        //lista_add.push(item.textContent)
+        //lista = [new Set(lista_add)]   
+        item.childNodes[1].textContent = parseInt(item.childNodes[1].textContent) + 1
+        console.log(item.childNodes[1].textContent)   
     })
 })
 
