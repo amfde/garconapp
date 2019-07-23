21574bd450fd0750bae8696a5bf455aa2d3b5143
Andre Franca <andrefranca@centro.intranet>
2019-07-23 13:26:17 -0300

    update

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,34 @@
 const itens = document.querySelectorAll('.item')
-const quantidade = document.querySelector('.qtd')
+const quantidade = document.querySelectorAll('.qtd')
 
 //let lista_add = []
 
-itens.forEach(item =>{
-    
+itens.forEach((item, index) =>{  
     item.addEventListener('click', (event)=>{              
         event.preventDefault
         //lista_add.push(item.textContent)
         //lista = [new Set(lista_add)]  
         //quantidade.firstChild.textContent = parseInt(quantidade.firstChild.textContent) + 1
-        console.log(item.firstChild.textContent)  
-        var nomeProduto = item.firstChild.textContent;
+        // colecao.forEach(cl =>{
+        //     valor = cl.lastElementChild.lastElementChild.firstChild.textContent
+        //     cl.lastElementChild.lastElementChild.firstChild.textContent = parseInt(cl.lastElementChild.lastElementChild.firstChild.textContent) + 1
+        //     console.log(valor)
+        // })         
+        quantidade[index].children[0].textContent = parseInt(quantidade[index].children[0].textContent) + 1 
+        let nomeProduto = item.firstChild.textContent
         Materialize.toast(nomeProduto + ' adicionado', 1100); 
     })
 })
 
-// quantidade.forEach((qtd)=>{
-//     qtd.addEventListener('click', (event)=>{
-        
-//         if(qtd.textContent > 0){
-//             qtd.textContent = parseInt(qtd.textContent) - 2
-//         }
-//     })
-// })
+quantidade.forEach((qtd, index)=>{
+    qtd.addEventListener('click', (event)=>{ 
+        console.log(qtd.children)   
+        if(qtd.firstChild.textContent > 0){
+            qtd.firstChild.textContent = parseInt(qtd.firstChild.textContent) - 1
+            let nomeProduto = itens[index].firstChild.textContent
+            Materialize.toast(nomeProduto + ' removido', 1100);
+        }
+    })
+})
 
 
